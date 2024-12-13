f1ed8983bb62ca05fb1790b6ca4559dc023fbf3a
1sjsicc <apriljoysalvador2018@gmail.com>
2024-12-13 11:18:37 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    const messageDiv = document.getElementById('message');
+    // Toggle the message visibility when the card is clicked
+    if (messageDiv.style.display === "none" || messageDiv.style.display === "") {
+        messageDiv.style.display = "block";
+    } else {
+        messageDiv.style.display = "none";
+    }
+}
